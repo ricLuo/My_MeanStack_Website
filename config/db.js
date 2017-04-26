43b567d66c367864151f4cf82c8ca7290ae3dd6e
ricace <vigric@outlook.com>
2017-04-26 11:45:57 -0400

    configure db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   // url : 'mongodb://localhost:27017/MyWebSite'
-  url: 'mongodb://ec2-54-173-241-209.compute-1.amazonaws.com:8080/MyWebSite'
+  url : 'mongodb://ricace:19891129@ec2-54-173-241-209.compute-1.amazonaws.com:27017/dummyDB'
 };
